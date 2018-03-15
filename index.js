0877aba8fd286c10842267ec9f7826cb88293479
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-15 05:46:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
 }
-function sayHiToGrandma(string) 
+function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return  ("I can't hear you!");
   }
@@ -19,4 +19,4 @@ function sayHiToGrandma(string)
   }
   if (string === `I love you, Grandma.`) {
     return ("I love you, too.")
-  }
+  }}
